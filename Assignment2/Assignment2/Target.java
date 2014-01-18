1ec79faa2e25a93fe219ec8b6c4b325c8742089b
Brian <fangbrian@gmail.com>
2014-01-17 23:27:34 -0800

    Target, but still need to get rid of outline

--- Assignment2/Assignment2/Target.java
+++ Assignment2/Assignment2/Target.java
@@ -11,7 +11,34 @@ import acm.program.*;
 import java.awt.*;
 
 public class Target extends GraphicsProgram {	
+	
+	//what happens when a double multiplies with int	
+	private static final int RADIUS_LARGE = 72;
+	private static final double MED_SCALING = 0.65;
+	private static final double SMALL_SCALING = 0.3;
+	
 	public void run() {
-		/* You fill this in. */
+		int x_dimen = getWidth();
+		int y_dimen = getHeight();
+		
+		drawRedCircle(x_dimen/2 - RADIUS_LARGE, y_dimen/2 - RADIUS_LARGE, RADIUS_LARGE*2, RADIUS_LARGE*2);
+		drawWhiteCircle(x_dimen/2 - MED_SCALING* RADIUS_LARGE, y_dimen/2 - MED_SCALING*RADIUS_LARGE, MED_SCALING*RADIUS_LARGE*2, MED_SCALING*RADIUS_LARGE*2);
+		drawRedCircle(x_dimen/2 - SMALL_SCALING* RADIUS_LARGE, y_dimen/2 - SMALL_SCALING*RADIUS_LARGE, SMALL_SCALING*RADIUS_LARGE*2, SMALL_SCALING*RADIUS_LARGE*2);
+		
+		
+	}
+	
+	private void drawRedCircle (double xPos, double yPos, double height, double width){
+		GOval red_circ = new GOval(xPos, yPos, height, width);
+		red_circ.setFilled(true);
+		red_circ.setFillColor(Color.RED);
+		add(red_circ);
+	}
+	
+	private void drawWhiteCircle (double xPos, double yPos, double height, double width){
+		GOval white_circ = new GOval(xPos, yPos, height, width);
+		white_circ.setFilled(true);
+		white_circ.setFillColor(Color.WHITE);
+		add(white_circ);
 	}
 }
