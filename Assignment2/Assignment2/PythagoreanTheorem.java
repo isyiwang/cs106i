a86bd1f9c4f14a4f66de411b02d8613faab47a1e
cliffkang <cliffgkang@gmail.com>
2014-02-20 23:49:02 -0800

    Pythagorean complete @isyiwang

--- Assignment2/Assignment2/PythagoreanTheorem.java
+++ Assignment2/Assignment2/PythagoreanTheorem.java
@@ -7,9 +7,19 @@
  */
 
 import acm.program.*;
+import java.lang.Math;
 
 public class PythagoreanTheorem extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
+		println("Enter values to compute Pythagorean theorem.");
+		int a = readInt("a: ");
+		int b = readInt("b: ");
+		double c = Math.sqrt(a*a + b*b);
+		println("c = " + c);
 	}
+	/*
+	 * I tried to think about test cases to fail it...but couldn't :\.
+	 * Non-integers, console rejects; negative numbers are fine since it's
+	 * squared; 0's don't crash it either.
+	 */
 }
