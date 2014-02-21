ee1617c26e914b02f3786c34518b5df4b68079e1
cliffkang <cliffgkang@gmail.com>
2014-02-20 21:17:47 -0800

    Target complete @isyiwang

--- Assignment2/Assignment2/Target.java
+++ Assignment2/Assignment2/Target.java
@@ -11,7 +11,36 @@ import acm.program.*;
 import java.awt.*;
 
 public class Target extends GraphicsProgram {	
+	private static final int OUTER_RADIUS = 72;
+	private static final double MIDDLE_RADIUS = 72*0.65;
+	private static final double INNER_RADIUS = 72*0.3;
+	
 	public void run() {
-		/*here we go*/
+		drawTarget(getWidth()/2, getHeight()/2);
+	}
+	private void drawTarget(double cx, double cy) {
+		drawOuter(cx - OUTER_RADIUS, cy - OUTER_RADIUS);
+		drawMiddle(cx - MIDDLE_RADIUS, cy - MIDDLE_RADIUS);
+		drawInner(cx - INNER_RADIUS, cy - INNER_RADIUS);
+	}
+	private void drawOuter(double x, double y) {
+		GOval outer = new GOval(x, y, 2*OUTER_RADIUS, 2*OUTER_RADIUS);
+		outer.setFilled(true);
+		outer.setFillColor(Color.RED);
+		add(outer);
+	}
+	private void drawMiddle(double x, double y) {
+		GOval middle = new GOval(x, y, 2*MIDDLE_RADIUS, 2*MIDDLE_RADIUS);
+		middle.setFilled(true);
+		middle.setFillColor(Color.WHITE);
+		middle.sendForward();
+		add(middle);
+	}
+	private void drawInner(double x, double y) {
+		GOval inner = new GOval(x, y, 2*INNER_RADIUS, 2*INNER_RADIUS);
+		inner.setFilled(true);
+		inner.setFillColor(Color.RED);
+		inner.sendToFront();
+		add(inner);
 	}
 }
